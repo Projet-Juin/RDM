da5c10d55ddc9d472cc075a0e15a98dbeb8c6122
Riton10 <Riton10@github.com>
2020-06-10 15:16:29 +0200

    fichier reprenant toutes les entrées

---
+++ appel _fonctions.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 15:07:13 2020
+
+@author: Forjot Henri
+"""
+
+
+### Liste de fonction d'Entrées ###
+# Longueur (mm) : L
+def longueur():
+    
+# Largeur section (mm) : b
+def largeur():
+    
+# Hauteur section (mm) : h
+def hauteur():
+    
+# Rayon de le section (mm) : R
+def rayon():
+    
+# Hauteur de l’aile supérieure (mm) : h1
+def hauteur_aile_sup():
+    
+# Hauteur de l’aile inférieure (mm) : h2
+def hauteur_aile_inf():
+    
+# Largeur de l’aile supérieure (mm) : b1
+def largeur_aile_sup():
+    
+# Largeur de l’aile inférieure (mm) : b2
+def largeur_aile_inf():
+    
+# Module de Young (N/mm²) : E
+def module__de_young():
+    
+# Limite élastique (MPa) : Re
+def limite_elastique():
+    
+# Charge concentrée (N) : p
+def charge_concentrée():
+    
+# Distance entre le noeud de base et l’endroit d’application de la charge concentrée (mm) : a1
+def a1():
+    
+# Charge répartie (N.m) : q
+def q():
+    
+# Distance sur laquelle la charge s’applique (mm) : l
+def l():
+    
+# Distance entre le noeud de base et l’endroit du début d’application de la charge (mm) : c1
+def c1():
+    
+# Moment (N.mm) : M
+def moment():
+    
+# Distance entre le noeud de base et l’endroit d’application du moment (mm) : e1
+def e1():
+    
