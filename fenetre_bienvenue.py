87bbe492596c5a043a65fcec12de4430d21d2ec2
Riton10 <Riton10@github.com>
2020-06-10 14:14:59 +0200

    dernière modif

--- fenetre_bienvenue.py
+++ fenetre_bienvenue.py
@@ -6,7 +6,6 @@ Created on Wed Jun 10 13:25:43 2020
 """
 
 from tkinter import *
-import main
 
 #Définition du visuel
 font_titre1 = ("Arial", 45, "bold")
@@ -19,19 +18,21 @@ gris_fonce='#9B9B9B'
 """
 création d'une fenetre de bienvenue
 """
-bienvenue = Tk() #création de la fenetre bienvenue
-bienvenue.title("RDM6+++ --- BIENVENUE") #Titre de l'encadré
-bienvenue.config(bg=gris_clair)
-bienvenue.resizable(width=FALSE, height=FALSE)
-bienvenue_message1=Label(bienvenue,fg="red",font=font_titre2,text='Bienvenue sur RDM6+++ qui casse la barraque',width=50, height=2,bg=gris_clair) #définit le message 1
-bienvenue_message1.pack() # affiche le message 1
-bienvenue_message2=Label(bienvenue,font=font_texte1,text='Ce programme à pour intérêt d\'étudier l\'efffet que peut avoir une charge sur une poutre.',wraplength=300,bg=gris_clair) #définit le message 2
-bienvenue_message2.pack() # affiche le message 2
-#Création d'un bouton pour entrer dans le programme
-def detruire_la_fenetre():
-    global bienvenue
-    bienvenue.destroy()
-    main()
-bouton_continuer = Button(bienvenue, text="Entrer",borderwidth=6,activebackground=gris_clair,relief="groove",command=detruire_la_fenetre)
-bouton_continuer.pack(anchor='se')
-bienvenue.mainloop()
+def fenetre_bienvenue():    
+    bienvenue = Tk() #création de la fenetre bienvenue
+    bienvenue.title("RDM6+++ --- BIENVENUE") #Titre de l'encadré
+    bienvenue.config(bg=gris_clair)
+    bienvenue.resizable(width=FALSE, height=FALSE)
+    bienvenue_message1=Label(bienvenue,fg="red",font=font_titre2,text='Bienvenue sur RDM6+++ qui casse la barraque',width=50, height=2,bg=gris_clair) #définit le message 1
+    bienvenue_message1.pack() # affiche le message 1
+    bienvenue_message2=Label(bienvenue,font=font_texte1,text='Ce programme à pour intérêt d\'étudier l\'efffet que peut avoir une charge sur une poutre.',wraplength=300,bg=gris_clair) #définit le message 2
+    bienvenue_message2.pack() # affiche le message 2
+    bouton_continuer = Button(bienvenue, text="Entrer",borderwidth=6,activebackground=gris_clair,relief="groove",command=bienvenue.destroy)
+    bouton_continuer.pack(anchor='se')
+    bienvenue.mainloop()
+
+# """
+# suppression de la fenetre_bienvenue
+# """   
+# def detruire_la_fenetre():
+#     bienvenue.destroy()
--- fenetre_main.py
+++ fenetre_main.py
@@ -6,11 +6,12 @@ Created on Wed Jun 10 13:38:00 2020
 """
 
 from tkinter import *
+from fenetre_bienvenue import fenetre_bienvenue
 
+#Définition du visuel
 font_titre1 = ("Arial", 45, "bold")
 font_titre2 = ("Arial", 14, "bold italic")
 font_texte1 = ("Arial", 8, "bold")
-
 gris_clair='#EDEDED'
 gris_fonce='#9B9B9B'
 
@@ -18,38 +19,44 @@ gris_fonce='#9B9B9B'
 """
 Création du main
 """
-def main():
-    #fenetre principale
-    main=Tk()
-    main.title("RDM6+++ --- Écran principal") #Titre de l'encadré
-    main.config(bg=gris_clair)
-    width = main.winfo_screenwidth() 
-    height = main.winfo_screenheight() 
-    main.geometry("%dx%d" % (width, height)) 
-    main.resizable(width=FALSE, height=FALSE)
-    
-    #encadré haut
-    top_frame= Frame(main, bg="red", width=1920, height=50)
-    
-    #encadré gauche
-    left_frame= Frame(main, bg="green", width=400, height=1030)
-    
-    #encadré droite
-    right_frame = Frame(main, bg='cyan', width =1520, height=1030)
+
+#fenetre principale
+main=Tk()
+main.title("RDM6+++ --- Écran principal") #Titre de l'encadré
+main.config(bg=gris_clair)
+width = main.winfo_screenwidth()  #obtient la taille de l'écran de l'utilisateur en largeur
+height = main.winfo_screenheight()  #obtient la taille de l'écran de l'utilisateur en hauteur
+main.geometry("%dx%d" % (width, height)) 
+# main.resizable(width=FALSE, height=FALSE) #empêche de déformer la fenetre
+
+#encadré haut
+top_frame= Frame(main, bg="red", width=1920, height=50)
+
+#encadré gauche
+left_frame= Frame(main, bg="green", width=400, height=1030)
+
+#encadré droite
+right_frame = Frame(main, bg='cyan', width =1520, height=1030)
 
 
 
-    #contenu top_frame
-    top_frame_message1=Label(top_frame,text='top',anchor=W,bg=gris_clair) #définit le message 1
-    
-    #contenu left_frame
-    left_frame_message1=Label(left_frame,text='left',width=50, height=2,bg=gris_clair) #définit le message 2
-    
-    #contenu right_frame
-    right_frame_message1=Label(right_frame,text='right',width=50, height=2,bg=gris_clair) #définit le message 3
+#contenu top_frame
+top_frame_message1=Label(top_frame,text='top',anchor=W,bg=gris_clair) #définit le message 1
+
+#contenu left_frame
+left_frame_message1=Label(left_frame,text='left',width=50, height=2,bg=gris_clair) #définit le message 2
+
+#contenu right_frame
+right_frame_message1=Label(right_frame,text='right',width=50, height=2,bg=gris_clair) #définit le message 3
 
 
-    top_frame.grid(row=0,column=0,columnspan=5)
-    left_frame.grid(row=1, column=0,columnspan=1)
-    right_frame.grid(row=1, column=1,columnspan=4)
-    main.mainloop()
+top_frame.grid(row=0,column=0,columnspan=5)
+left_frame.grid(row=1, column=0,columnspan=1)
+right_frame.grid(row=1, column=1,columnspan=4)
+
+
+#lancer le main
+main.mainloop()
+
+#import le bienvenue
+fenetre_bienvenue()
