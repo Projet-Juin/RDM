2bfa7f67ca1792a65eebaf64d9e15cd7eea6ab04
Riton10 <henri.forjot@epfedu.fr>
2020-06-22 09:30:39 +0200

    modif veille couleur restante

--- INTERFACE_Annexe_1.py
+++ INTERFACE_Annexe_1.py
@@ -15,8 +15,7 @@ import os
 font_titre1 = ("Arial", 45, "bold")
 font_titre2 = ("Arial", 14, "bold italic")
 font_texte1 = ("Arial", 8, "bold")
-gris_clair='#EDEDED'
-gris_fonce='#9B9B9B'
+gris_5='#5D6D7E'
 
 def donothing(): # Pour eviter les bugs, fonctions qui dit que rien n'est encore codé quand on sélectionne un élts non codés
     nouvelle_fenetre =Tk()
@@ -33,13 +32,13 @@ def donothing_event(event): # Pour eviter les bugs, fonctions qui dit que rien n
 def fenetre_bienvenue(): #Création d'une fenetre de bienvenue    
     bienvenue = Tk() #création de la fenetre bienvenue
     bienvenue.title("RDM6+++ --- BIENVENUE") #Titre de l'encadré
-    bienvenue.config(bg=gris_clair)
+    bienvenue.config(bg=gris_5)
     bienvenue.resizable(width=FALSE, height=FALSE)
-    bienvenue_message1=Label(bienvenue,fg="red",font=font_titre2,text='Bienvenue sur RDM6+++ qui casse la barraque',width=50, height=2,bg=gris_clair) #définit le message 1
+    bienvenue_message1=Label(bienvenue,fg="red",font=font_titre2,text='Bienvenue sur RDM6+++ qui casse la barraque',width=50, height=2,bg=gris_5) #définit le message 1
     bienvenue_message1.pack() # affiche le message 1
-    bienvenue_message2=Label(bienvenue,font=font_texte1,text='Ce programme à pour intérêt d\'étudier l\'efffet que peut avoir une charge sur une poutre.',wraplength=300,bg=gris_clair) #définit le message 2
+    bienvenue_message2=Label(bienvenue,font=font_texte1,text='Ce programme à pour intérêt d\'étudier l\'efffet que peut avoir une charge sur une poutre.',wraplength=300,bg=gris_5) #définit le message 2
     bienvenue_message2.pack() # affiche le message 2
-    bouton_continuer = Button(bienvenue, text="Entrer",borderwidth=6,activebackground=gris_clair,relief="raised",overrelief="groove",command=bienvenue.destroy)
+    bouton_continuer = Button(bienvenue, text="Entrer",borderwidth=6,activebackground=gris_5,relief="raised",overrelief="groove",command=bienvenue.destroy)
     bouton_continuer.pack(anchor='se')
     bienvenue.mainloop()
 
